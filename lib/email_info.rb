4719eea520b5b84dab53a47671b3e050854bbb5d
Gregory N. M <gregory.nicholas.mayer@hotmail.com>
2023-05-03 23:53:48 -0300

    add class to call the api and store the response info

---
+++ lib/email_info.rb
@@ -0,0 +1,9 @@
+
+class EmailInfo
+  attr_reader :email, :status
+
+  def initialize email, status
+    @email = email
+    @status = status
+  end
+end
---
+++ lib/verify_emails.rb
@@ -0,0 +1,46 @@
+require 'dotenv/load'
+Dotenv.load('.env')
+require 'net/http'
+require 'uri'
+require 'json'
+require_relative 'email_info.rb'
+
+class VerifyEmails
+  
+  
+  API_KEY = ENV['BOUNCER_API_KEY']
+  API_ENDPOINT = 'https://api.usebouncer.com/v1.1/email/verify?email='
+  attr_accessor :emails
+  
+  def initialize emails
+      @emails = emails
+  end
+
+  def validate
+    verified_emails = Array.new
+    emails.each do |email|
+      response = request email
+      data = filter response
+      verified_emails << EmailInfo.new(data['email'], data['status'])
+    end
+    verified_emails
+  end
+
+  private
+  def request email
+    uri = URI.parse("#{API_ENDPOINT}#{email}")
+    request = Net::HTTP::Get.new(uri)
+    request["x-Api-Key"] = API_KEY
+    req_options = { use_ssl: uri.scheme == "https" }
+    response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+      http.request(request)
+    end
+  end
+
+  def filter response
+    JSON
+      .parse(response.body)
+      .slice("email", "status")
+  end
+
+end
