35130a912f7d8134a1f5f5e0286ecdd9eadae1c7
Gregory N. M <gregory.nicholas.mayer@hotmail.com>
2023-05-03 23:51:20 -0300

    add class to handle reading emails from the csv file

---
+++ lib/read_csv.rb
@@ -0,0 +1,17 @@
+require 'csv'
+require 'pry'
+require_relative 'verify_emails.rb'
+class ReadCsv
+
+  def initialize filename
+    @csv = CSV.new(File.read(filename), headers: true)
+  end
+
+  def read
+    @csv.map do |row|
+      row = row.to_h
+      row['email']
+    end
+  end
+
+end
